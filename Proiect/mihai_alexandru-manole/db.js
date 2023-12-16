8c9225c46a8c5f2ecc834ff9737a7dd4f08cb22c
alexmanole87 <42910029+alexmanole87@users.noreply.github.com>
2023-12-16 18:31:49 +0200

    Delete Proiect directory

--- Proiect/mihai_alexandru-manole/db.js
+++
@@ -1,9 +0,0 @@
-//const Sequelize = require('sequelize');
-import {Sequelize} from "sequelize";
-
-const sequelize = new Sequelize('candidati', 'root', 'Arra1987#', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
-
-export default sequelize;
